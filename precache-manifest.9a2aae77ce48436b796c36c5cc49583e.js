95a530fe1451eacd777be89abd1346822284690d
Giampiero Lai <giampiero.lai.external@ynap.com>
2020-07-15 21:43:00 +0200

    Updates

---
+++ precache-manifest.9a2aae77ce48436b796c36c5cc49583e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7baaf7ea9e2671d2f747a4fa32d4ef09",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "d0a34c2634d492d9e1ce",
+    "url": "/simple-todo-app/static/css/main.d3faecd3.chunk.css"
+  },
+  {
+    "revision": "517448722c259d425d02",
+    "url": "/simple-todo-app/static/js/2.bd25b82b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.bd25b82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a34c2634d492d9e1ce",
+    "url": "/simple-todo-app/static/js/main.ea1952bb.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
